032f4d13e318b8488d7e17ea47006f5c0bb479ec
MatheusY <matheus.matsubara12@gmail.com>
2018-09-01 19:13:53 -0300

    inicio do projeto

---
+++ src/main/java/TestaCache.java
@@ -0,0 +1,27 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import br.com.caelum.JpaConfigurator;
+import br.com.caelum.model.Produto;
+
+public class TestaCache {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(JpaConfigurator.class);
+		EntityManagerFactory entityManagerFatory = ctx.getBean(EntityManagerFactory.class);
+		
+		EntityManager em = entityManagerFatory.createEntityManager();
+		Produto produto = em.find(Produto.class, 1);
+		Produto produto2 = em.find(Produto.class, 1);
+		
+		System.out.println(produto.getNome());
+		System.out.println(produto2.getNome());
+		
+		em.close();
+		
+		
+	}
+}
